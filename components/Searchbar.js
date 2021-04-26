b832837df6913c0496288927ed6032ed2f44f6b2
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-04-26 12:16:16 +0200

    Minor fix

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -5,16 +5,16 @@ const Searchbar = ({
   setSearchString,
 }) => {
   return (
-    <form onSubmit={handleSearch}  className='flex flex-col m-5'>
+    <form onSubmit={handleSearch}  className='m-5 flex flex-col md:flex-row'>
       <input
         type='text'
-        className='text-4xl'
+        className='text-4xl md:w-3/4'
         autoFocus
         placeholder='Search for music'
         value={searchString}
         onChange={(e) => setSearchString(e.target.value)}
         />
-      <div className='mt-2 flex justify-end'>
+      <div className='mt-2 ml-5 flex justify-end md:items-center'>
         <button type='submit' className='p-2' onClick={handleSearch}>
           Search
         </button>
