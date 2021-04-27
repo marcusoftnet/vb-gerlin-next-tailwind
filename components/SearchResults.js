53448c60a02b1256cb079ff7c500efed13deaea8
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-04-27 08:52:27 +0200

    First layout of searchresult done

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,8 +1,33 @@
+import { useRouter } from 'next/router';
+
 const SearchResults = ({ searchResult }) => {
+  const router = useRouter();
+  const materials = searchResult?.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  const showMaterial = (materialId) => router.push(`/material/${materialId}`);
+
   return (
-    <div>
-      {searchResult?.docs.map((doc) => (
-        <div key={doc.id}>{doc.data().title}</div>
+    <div className='ml-5'>
+      <div className='grid grid-flow-col grid-cols-4 gap-4'>
+        <div className='text-2xl'>Number</div>
+        <div className='text-2xl'>Type</div>
+        <div className='text-2xl'>Title</div>
+        <div className='text-2xl'>Composer</div>
+      </div>
+      {materials?.map((material) => (
+        <div
+          key={material.id}
+          className='grid grid-flow-col grid-cols-4 gap-4 border-b border-gray-400 cursor-pointer'
+          onClick={() => showMaterial(material.id)}
+        >
+          <div>{material.seriesNumber}</div>
+          <div>{material.type}</div>
+          <div>{material.title}</div>
+          <div>{material.composer}</div>
+        </div>
       ))}
     </div>
   );
